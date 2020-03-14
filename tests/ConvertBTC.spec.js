e9f45fe014189798b1c2fd21eaa6b56efd212300
Luiz Claudio <luizcns18@gmail.com>
2020-03-14 09:29:42 -0300

    Handling errors

--- tests/ConvertBTC.spec.js
+++ tests/ConvertBTC.spec.js
@@ -53,4 +53,32 @@ describe('ConvertBTC', () => {
       done();
     }, 300);
   });
+
+  it('should use currency BRL and 1 as amount default', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'BRL', amount: 1 })
+      .reply(200, responseMock);
+
+    convertBTC('BRL');
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith('1 BTC to BRL = 2490.78');
+      done();
+    }, 300);
+  });
+
+  it('should message user when reply with error', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'BRL', amount: 1 })
+      .replyWithError('Error');
+
+    convertBTC('BRL');
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith('Something went wrong in the API. Try in a few minutes');
+      done();
+    }, 300);
+  });
 });
